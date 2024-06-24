768f85af8e20ee74687edc8f6f97c32f16228153
Nicolas Leigh <linze201@gmail.com>
2024-06-24 17:45:23 +0800

    using http.FileServer to serve static files

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,6 +9,10 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	// Check if the current request URL path exactly matches "/". If it doesn't, use     
+	// the http.NotFound() function to send a 404 response to the client.
+  // Importantly, we then return from the handler. If we don't return the handler     
+	// would keep executing and also write the "Hello from SnippetBox" message.
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -18,8 +22,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// to note that the file containing our base template must be the *first*
 	// file in the slice.
 	files := []string{
-		"./ui/html/base.gohtml",
-		"./ui/html/pages/home.gohtml",
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+		"./ui/html/partials/nav.tmpl",
 	}
 
 	// Use the template.ParseFiles() function to read the template file into a
@@ -29,7 +34,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	// Use the template.ParseFiles() function to read the files and store the
 	// templates in a template set. Notice that we can pass the slice of file
-	// paths as a variadic parameter?
+	// paths as a variadic parameter
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
@@ -53,14 +58,22 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Write([]byte("Hello from Snippetbox"))
 }
+
 func snippetView(w http.ResponseWriter, r *http.Request) {
+	// Extract the value of the id parameter from the query string and try to     
+	// convert it to an integer using the strconv.Atoi() function. If it can't     
+	// be converted to an integer, or the value is less than 1, we return a 404 page     
+	// not found response.
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
+	// Use the fmt.Fprintf() function to interpolate the id value with our response     
+	// and write it to the http.ResponseWriter.
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
+
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,10 +6,24 @@ import (
 )
 
 func main() {
+	// Use the http.NewServeMux() function to initialize a new servemux, then     
+	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
+
+	// Create a file server which serves files out of the "./ui/static" directory.
+  // Note that the path given to the http.Dir function is relative to the project     
+	// directory root.
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/",http.StripPrefix("/static/",fileServer))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	// Use the http.ListenAndServe() function to start a new web server. We pass in     
+	// two parameters: the TCP network address to listen on (in this case ":4000")     
+	// and the servemux we just created. If http.ListenAndServe() returns an error     
+	// we use the log.Fatal() function to log the error message and exit. Note     
+	// that any error returned by http.ListenAndServe() is always non-nil.
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
