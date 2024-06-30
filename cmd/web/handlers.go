67451c3c7efba295543a395294b53ecd83af8ec9
Nicolas Leigh <linze201@gmail.com>
2024-06-30 10:59:14 +0800

    parsing form data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -173,7 +173,8 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display form"))
+	data := app.newTemplateData(r)
+	app.render(w, http.StatusOK, "create.tmpl", data)
 }
 
 // Change the signature of the snippetCreate handler so it is defined as a method
@@ -191,9 +192,32 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	}
 	*/
 
+	// First we call r.ParseForm() which adds any data in POST request bodies to the r.PostForm map. This also works in the same way for PUT and PATCH requests. 
+	// If there are any errors, we use our app.ClientError() helper to send a 400 Bad Request response to the user.
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// Use the r.PostForm.Get() method to retrieve the title and content from the r.PostForm map.
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+
+	// The r.PostForm.Get() method always returns the form data as a *string*.
+  // However, we're expecting our expires value to be a number, and want to represent it in our Go code as an integer. 
+	// So we need to manually covert the form data to an integer using strconv.Atoi(), and we send a 400 Bad Request response if the conversion fails.
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return 
+	}
+
+	/* 
 	title := "O snail"
 	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
 	expires := 7
+	*/
 
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
